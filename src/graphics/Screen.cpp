cb848dccb96ea7501afb8cb1e654d7ee24dfdc29
humbertovnavarro <humberto.navarro.dev@gmail.com>
2025-02-10 18:27:58 -0800

    fix pepe

--- src/graphics/Screen.cpp
+++ src/graphics/Screen.cpp
@@ -162,8 +162,6 @@ static void drawIconScreen(const char *upperMsg, OLEDDisplay *display, OLEDDispl
 
     display->setFont(FONT_MEDIUM);
     display->setTextAlignment(TEXT_ALIGN_LEFT);
-    const char *title = "meshtastic.org";
-    display->drawString(x + getStringCenteredX(title), y + SCREEN_HEIGHT - FONT_HEIGHT_MEDIUM, title);
     display->setFont(FONT_SMALL);
 
     // Draw region in upper left
